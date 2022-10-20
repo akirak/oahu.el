fa0aa86a71177329e2cd9d8b2ba7e694ad671bd7
Akira Komamura <akira.komamura@gmail.com>
2022-10-20 11:30:01 +0900

    Add a library header

--- oahu.el
+++ oahu.el
@@ -1,5 +1,36 @@
 ;;; oahu.el --- Manage Org searches -*- lexical-binding: t -*-
 
+;; Copyright (C) 2022 Akira Komamura
+
+;; Author: Akira Komamura <akira.komamura@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((emacs "28.1") (org "9.5"))
+;; Keywords: matching
+;; URL: https://git.sr.ht/~akirak/oahu.el
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
 ;;;; Macros to help define processes
 
 ;; Put the same definition in the autoload to allow use of the macro in the
