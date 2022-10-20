89a731e8cea13f8f56416c4354216583ad54204d
Akira Komamura <akira.komamura@gmail.com>
2022-10-20 12:04:19 +0900

    Add oahu-memento.el

---
+++ extra/oahu-memento.el
@@ -0,0 +1,38 @@
+;;; oahu-memento.el --- Org-Memento integration for oahu -*- lexical-binding: t -*-
+
+(require 'org)
+(require 'org-memento)
+(require 'oahu)
+
+(defvar oahu-last-view)
+
+;;;###autoload
+(defun oahu-memento-save ()
+  "Save the last view to the current block entry."
+  (when oahu-last-view
+    (org-memento-with-current-block
+      ;; Save to separate properties as the structure of `oahu-last-view' may
+      ;; change in the future.
+      (org-entry-put nil "oahu_process_name" (nth 0 oahu-last-view))
+      (org-entry-put nil "oahu_process_argument"
+                     (let ((print-level nil)
+                           (print-circle nil)
+                           (print-length nil))
+                       (prin1-to-string (nth 1 oahu-last-view))))
+      (org-entry-put nil "oahu_view_name" (nth 2 oahu-last-view)))))
+
+;;;###autoload
+(defun oahu-memento-load ()
+  "Load a view from the current block entry."
+  (setq oahu-last-view (oahu-memento--view-from-entry)))
+
+(defun oahu-memento--view-from-entry ()
+  (when-let* ((alist (org-memento-with-current-block
+                       (org-entry-properties nil 'standard)))
+              (process (cdr (assoc "OAHU_PROCESS_NAME" alist)))
+              (argument (read (cdr (assoc "OAHU_PROCESS_ARGUMENT" alist))))
+              (view (cdr (assoc "OAHU_VIEW_NAME" alist))))
+    (list process argument view)))
+
+(provide 'oahu-memento)
+;;; oahu-memento.el ends here
