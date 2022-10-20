0fd5e9f2599cbbb05c63fba9629e5946317069c9
Akira Komamura <akira.komamura@gmail.com>
2022-10-20 12:56:19 +0900

    Add a library header

--- extra/oahu-memento.el
+++ extra/oahu-memento.el
@@ -1,5 +1,36 @@
 ;;; oahu-memento.el --- Org-Memento integration for oahu -*- lexical-binding: t -*-
 
+;; Copyright (C) 2022 Akira Komamura
+
+;; Author: Akira Komamura <akira.komamura@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((emacs "28.1") (oahu "0.1") (org-memento "0.1"))
+;; Keywords:
+;; URL: https://git.sr.ht/~akirak/oahu.el
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
 (require 'org)
 (require 'org-memento)
 (require 'oahu)
