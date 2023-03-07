63743485c96b6bcaba97bd2008eb5d796d4da7ab
Akira Komamura <akira.komamura@gmail.com>
2023-03-07 13:50:12 +0900

    feat: Re-run block hook if the target block is the current block

--- extra/oahu-memento.el
+++ extra/oahu-memento.el
@@ -97,7 +97,13 @@ interactively."
                (not (org-before-first-heading-p)))
     (user-error "You must run this command inside the buffer of org-memento-file"))
   (apply #'oahu-memento--save-to-entry
-         (oahu-read-context-globally)))
+         (oahu-read-context-globally))
+  (when (and (equal org-memento-current-block (org-get-heading t t t t))
+             (org-with-wide-buffer
+              (and (re-search-backward (rx bol "*" blank) nil t)
+                   (equal (match-string 4)
+                          (org-memento--today-string)))))
+    (org-memento-rerun-block-hook)))
 
 (defun oahu-memento--save-to-entry (type argument &optional view-name)
   (org-entry-put nil "OAHU_PROCESS_NAME" (symbol-name type))
